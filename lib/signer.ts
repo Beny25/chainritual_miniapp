c919f3b04162e2734af51fa187fd2a7f448a9aa5
beny25 <bnhartanto@gmail.com>
2025-12-09 11:05:26 +0700

    Fix signer using noble hashes utils

--- lib/signer.ts
+++ lib/signer.ts
@@ -1,8 +1,13 @@
 import * as ed from "@noble/ed25519";
+import { hexToBytes } from "@noble/hashes/utils";
 
 export async function signMessage(message: Uint8Array, privateKey: string) {
-  const pkBytes = ed.utils.hexToBytes(privateKey);
+  // Convert hex → Uint8Array
+  const pkBytes = hexToBytes(privateKey);
+
+  // noble-ed25519 expects Uint8Array private key
   const signature = await ed.sign(message, pkBytes);
+
   return signature;
 }
 
